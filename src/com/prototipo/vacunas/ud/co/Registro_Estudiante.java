fa5bc3c4f00dd5c35047b8ac2e242a042c209362
JonathanMendez-UD <JonathanMendez-UD@users.noreply.github.com>
2021-01-08 23:53:35 -0500

    Registro_Estudiante

---
+++ src/com/prototipo/vacunas/ud/co/Registro_Estudiante.java
@@ -0,0 +1,11 @@
+package com.prototipo.vacunas.ud.co;
+
+import javax.swing.*;
+
+public class Registro_Estudiante {
+    private JTextField textField1;
+    private JTextField textField2;
+    private JTextField textField3;
+    private JSpinner spinner1;
+    private JButton siguienteButton;
+}
