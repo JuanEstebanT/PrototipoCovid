b0b13020ad15e64f8e50c27939925ba350dd9fd8
David G <65410396+kdavid1998@users.noreply.github.com>
2021-01-09 22:54:51 -0500

    Registro_Funcionario v2

--- src/com/prototipo/vacunas/ud/co/Registro_Funcionario.java
+++ src/com/prototipo/vacunas/ud/co/Registro_Funcionario.java
@@ -1,4 +1,15 @@
 package com.prototipo.vacunas.ud.co;
 
+import javax.swing.*;
+
 public class Registro_Funcionario {
+    private JPanel panel1;
+    private JTextField textField5;
+    private JTextField textField1;
+    private JTextField textField3;
+    private JTextField textField4;
+    private JComboBox comboBox1;
+    private JButton siguienteButton;
+    private JComboBox comboBox2;
+    private JComboBox comboBox3;
 }
