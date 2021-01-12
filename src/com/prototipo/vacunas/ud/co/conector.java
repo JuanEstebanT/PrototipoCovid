fb244c6198bc46e61bee82ca58b39abc09799951
lolita <jetorresa@udistrital.edu.co>
2021-01-12 15:49:38 -0500

    xd

---
+++ src/com/prototipo/vacunas/ud/co/conector.java
@@ -0,0 +1,17 @@
+package com.prototipo.vacunas.ud.co;
+import javax.swing.*;
+import java.sql.*;
+public class conector {
+    Connection con;
+    public Connection conxion(){
+
+        try{
+            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/","root","");
+            JOptionPane.showMessageDialog(null,"Conexion establecida");
+        }catch (Exception e){
+            JOptionPane.showMessageDialog(null,"Conexion no establecida");
+        }
+
+        return con;
+    }
+}
