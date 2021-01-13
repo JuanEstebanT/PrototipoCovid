6e91e0318b5be53a30a021c8223068f696bb916d
lolita <jetorresa@udistrital.edu.co>
2021-01-13 17:54:31 -0500

    correcion 2.0

--- src/controlador/Controlador_estudiante.java
+++ src/controlador/Controlador_estudiante.java
@@ -18,37 +18,26 @@ public class Controlador_estudiante {
     private modelo_estudiante objme;
     private Registro_Estudiante objre;
 
-    Registro_Estudiante view_Est = new Registro_Estudiante();
-    modelo_estudiante model_Est= new modelo_estudiante();
-    Ventana_Inicial inicio_prin = new Ventana_Inicial();
 
-    public Controlador_estudiante(Registro_Estudiante view_Est, modelo_estudiante model_Est, personas objp){
-        this.view_Est= view_Est;
-        this.model_Est= model_Est;
+    public Controlador_estudiante(Registro_Estudiante objre, modelo_estudiante objme, personas objp){
+        this.objre= objre;
+        this.objme= objme;
         this.objp = objp;
-        this.objme = objme;
-        this.objre = objre;
         this.objre.finalizar_EstButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 if(e.getSource()== objre.finalizar_EstButton){
 
                     objp.setCodigo (objre.txt_codigoEst.getText());
-                        objp.setNombre (objre.txt_nombreEst.getText());
-                            objp.setGenero (objre.box_generoEst.getSelectedItem().toString());
-                                objp.setDireccion(objre.txt_direccionEst.getText());
-                                    objp.setCorreo(objre.txt_correoEst.getText());
-                                        objp.setFacultad(objre.BoxFacultadEst.getSelectedItem().toString());
-                                            objp.setCargo("Estudiante");
-                                               objp.setVacunado("No vacunado");
+                    objp.setNombre (objre.txt_nombreEst.getText());
+                    objp.setGenero (objre.box_generoEst.getSelectedItem().toString());
+                    objp.setDireccion(objre.txt_direccionEst.getText());
+                    objp.setCorreo(objre.txt_correoEst.getText());
+                    objp.setFacultad(objre.BoxFacultadEst.getSelectedItem().toString());
+                    objp.setCargo("Estudiante");
+                    objp.setVacunado("No vacunado");
 
-                                               if (objme.RegistrarEst(objp))
-                                               {
-                                                   JOptionPane.showMessageDialog(null, "Registro Exitoso");
-                                               }
-                                               else {
-                                                   JOptionPane.showMessageDialog(null, "Registro Exitoso");
-                                               }
+                    JOptionPane.showMessageDialog(null, "Registro Exitoso");
                 }
 
             }
