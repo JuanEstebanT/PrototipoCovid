7f83f51819d0a7e88f5cf9e679b58c34a340cc12
JonathanMendez-UD <JonathanMendez-UD@users.noreply.github.com>
2021-01-16 21:04:49 -0500

    Arreglada la tabla, falta agregar boton buscar y conectar al boton de ventana principal

--- src/vista/Ventana_Vacunar.java
+++ src/vista/Ventana_Vacunar.java
@@ -1,11 +1,18 @@
 package vista;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class Ventana_Vacunar {
+public class Ventana_Vacunar extends  JFrame {
     public JTable tabla_vacunar;
     private JButton Vacunar_vacunarbutton;
     private JTextField textField1;
     private JButton Vacunar_buscarbutton;
+    private JPanel panel1;
 
+    public Ventana_Vacunar (){
+        setContentPane(panel1);
+
+    }
 }
