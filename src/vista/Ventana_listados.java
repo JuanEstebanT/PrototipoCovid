9ab2a618bfd3011c401e30c2e1841afc2b6cf8ab
lolita <jetorresa@udistrital.edu.co>
2021-01-16 20:14:07 -0500

    ventana de listados bonita

--- src/vista/Ventana_listados.java
+++ src/vista/Ventana_listados.java
@@ -1,4 +1,21 @@
 package vista;
 
+import javax.swing.*;
+
 public class Ventana_listados {
+    private JPanel panel1;
+    private JTable table1;
+    private JTextField txt_nombreMDF;
+    private JTextField txt_codigoMDF;
+    private JTextField txt_generoMDF;
+    private JTextField txt_direccionMDF;
+    private JTextField txt_correoMDF;
+    private JTextField txt_facultadMDF;
+    private JTextField txt_cargoMDF;
+    private JTextField txt_puntajeMDF;
+    private JTextField txt_vacunadoMDF;
+    private JTextField txt_buscarMDF;
+    private JButton buscarButton;
+    private JButton modificarButton;
+    private JButton eliminarButton;
 }
